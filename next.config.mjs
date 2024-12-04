f02d3fa05150cec3462ad98d450d08402ce25eb0
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-12-05 03:32:57 +0800

    Update next.config.mjs
    fails

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/baidu",
+        destination: "https://baidu.com",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
